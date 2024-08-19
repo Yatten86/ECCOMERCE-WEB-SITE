a42760d0fa6f7a28286c2258708de5b0e53585b2
Yatten86 <cosmoreddog@gmail.com>
2024-08-20 00:39:49 +0300

    req-reset-passowrd-pages

--- vue-js-admin/src/views/ResetPassword.vue
+++ vue-js-admin/src/views/ResetPassword.vue
@@ -3,15 +3,10 @@
         class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
     >
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                class="mx-auto h-10 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-            />
             <h2
                 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
             >
-                Sign in to your account
+                Set new password
             </h2>
         </div>
 
@@ -19,16 +14,15 @@
             <form class="space-y-6" action="#" method="POST">
                 <div>
                     <label
-                        for="email"
+                        for="new-password"
                         class="block text-sm font-medium leading-6 text-gray-900"
-                        >Email address</label
+                        >New Password</label
                     >
                     <div class="mt-2">
                         <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autocomplete="email"
+                            id="new-password"
+                            name="password"
+                            type="password"
                             required=""
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
@@ -38,27 +32,31 @@
                 <div>
                     <div class="flex items-center justify-between">
                         <label
-                            for="password"
+                            for="repeat-password"
                             class="block text-sm font-medium leading-6 text-gray-900"
-                            >Password</label
+                            >Repeat Password</label
                         >
-                        <div class="text-sm">
-                            <router-link
-                                :to="{ name: 'requestPassword' }"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >Forgot password?</router-link
-                            >
-                        </div>
                     </div>
                     <div class="mt-2">
                         <input
-                            id="password"
-                            name="password"
+                            id="repeat-password"
+                            name="password_repeat"
                             type="password"
-                            autocomplete="current-password"
                             required=""
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
+                    </div>
+                </div>
+
+                <div>
+                    <div class="flex items-center justify-between">
+                        <div class="text-sm">
+                            <router-link
+                                :to="{ name: 'login' }"
+                                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >Go back to Login</router-link
+                            >
+                        </div>
                     </div>
                 </div>
 
@@ -67,7 +65,7 @@
                         type="submit"
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        Sign in
+                        Submit
                     </button>
                 </div>
             </form>
@@ -75,8 +73,4 @@
     </div>
 </template>
 
-<script>
-export default {
-    name: "Login",
-};
-</script>
+<script setup></script>
